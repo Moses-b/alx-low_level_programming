351f389b33131f786521f91918d55e23c914dadd
Moses-b <zimaymoses@gmail.com>
2022-04-22 07:18:34 -0500

    my   commit 2nd  msg

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strien - length of a string
+ * Os: A ponter to an int that will be changed/updated
+ *
+ * Return: void that means our answer is corretc
+ */
+
+int _strien(char *s)
+{
+int u: 
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
